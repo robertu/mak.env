2f2184b584498e734f22b58f17ad513237c5db99
robertu <robertu@robertu.net>
2021-04-02 21:38:00 +0200

    just a lib

---
+++ libs.sh
@@ -0,0 +1,24 @@
+apt-get install -y \
+build-essential \
+libbz2-dev \
+libfreetype6 \
+libfreetype6-dev \
+libjpeg-dev \
+libmemcached-dev \
+libncurses5-dev libncurses5 \
+libpng-dev \
+libpq-dev \
+libsqlite3-0 \
+libsqlite3-dev \
+libssl-dev openssl \
+libz-dev \
+zlib1g zlib1g-dev \
+tcl-dev tcllib tk-dev tklib \
+libpcre3 libpcre3-dev \
+curl \
+libaio-dev libaio1 \
+supervisor git mc vim neovim \
+libtiff-tools \
+libpacklib-lesstif1-dev \
+libffi-dev \
+postgresql-common postgresql postgresql-client-11 postgresql-contrib 
